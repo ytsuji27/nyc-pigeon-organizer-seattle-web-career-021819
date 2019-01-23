a835c4313208b7203244ac337e0abaec44278fb7
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-23 01:18:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,24 +1,5 @@
 require 'pry'
 
-  pigeon_data = {
-    :color => {
-      :purple => ["Theo", "Peter Jr.", "Lucky"],
-      :grey => ["Theo", "Peter Jr.", "Ms. K"],
-     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-      :brown => ["Queenie", "Alex"]
-    },
-    :gender => {
-      :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-      :female => ["Queenie", "Ms. K"]
-    },
-    :lives => {
-      "Subway" => ["Theo", "Queenie"],
-      "Central Park" => ["Alex", "Ms. K", "Lucky"],
-      "Library" => ["Peter Jr."],
-     "City Hall" => ["Andrew"]
-    }
-  }
-
 def nyc_pigeon_organizer
   
   pigeon_data = {
