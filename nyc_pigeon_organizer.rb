f4e0588af712225b813f23259f0409f868e3fbe5
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-23 01:14:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,6 +24,7 @@ def nyc_pigeon_organizer(data)
   data.each do |attributes, hash|
     hash.each do |characteristic, array|
       array.each do |name|
+        binding.pry
         if pigeon_list.has_key?(name)
           pigeon_list[name][attributes] << characteristic.to_s
         else
